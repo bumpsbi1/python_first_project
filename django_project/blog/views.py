8d63b2d107c9ccbc88f57fe7c8b13c567827f2c7
Huster Barnes <bumpsbitsnbytes@gmail.com>
2019-12-30 08:14:40 -0500

    Added stucture to the database and allow the site to present the information from the database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jumpy',
-        'title':'Blog Post 1',
-        'content':'Nisi Lorem non sint sit exercitation reprehenderit laboris ut laborum. Nulla anim irure commodo sit consequat. Et cillum cupidatat proident enim tempor excepteur ullamco est amet irure et ipsum. Ad sint ut elit tempor ad occaecat culpa sit.',
-        'date_posted':'August 27, 2019'
-    },
-    {
-        'author': 'Zippy',
-        'title':'Blog Post 2',
-        'content':'Minim ea cupidatat ex Lorem ipsum duis aliquip sint deserunt voluptate. Consequat consectetur consequat magna laboris commodo amet adipisicing. Aliquip anim incididunt fugiat ex aliqua quis quis cillum ut. Laboris occaecat dolor culpa amet pariatur. Fugiat proident aliqua ut consectetur id in officia cillum irure. Lorem minim velit consequat quis duis. Ut pariatur anim adipisicing in proident sunt eiusmod.',
-        'date_posted':'September 27, 2019'
-    }
-    
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
