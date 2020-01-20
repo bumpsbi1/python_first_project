f762ce9b0ec0383e8db6889110c3fcd6a51dbc71
Huster Barnes <bumpsbitsnbytes@gmail.com>
2020-01-20 12:26:42 -0500

    change the post functionality create update delete

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or image.width > 300:
+        if img.height > 300 or img.width > 300:
             output.size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.pathe)
+            img.save(self.image.path)
